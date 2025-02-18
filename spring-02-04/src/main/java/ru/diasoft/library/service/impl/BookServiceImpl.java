00c4cd2244a310a766e95fc7ae55c430b1ce9b75
Dmitrii Kosurov <dkosurov@diasoft.ru>
2025-02-19 00:44:15 +0300

    update repositories

--- spring-02-04/src/main/java/ru/diasoft/library/service/impl/BookServiceImpl.java
+++ spring-02-04/src/main/java/ru/diasoft/library/service/impl/BookServiceImpl.java
@@ -2,6 +2,7 @@ package ru.diasoft.library.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.diasoft.library.exception.BookNotFoundException;
 import ru.diasoft.library.repository.BookRepository;
 import ru.diasoft.library.domain.Author;
 import ru.diasoft.library.domain.Book;
@@ -40,7 +41,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book getById(long id) {
-        return bookRepository.getById(id);
+        return bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
     }
 
     @Override
@@ -53,11 +54,11 @@ public class BookServiceImpl implements BookService {
     public Book update(BookRequestDto bookDto) {
         Author author = authorService.findByFullNameOrCreate(bookDto.getAuthorFullName());
         Genre genre = genreService.findByNameOrCreate(bookDto.getGenre());
-        Book existedBook = bookRepository.getById(bookDto.getId());
+        Book existedBook = bookRepository.findById(bookDto.getId()).orElseThrow(BookNotFoundException::new);
         existedBook.setTitle(bookDto.getTitle());
         existedBook.setAuthor(author);
         existedBook.setGenre(genre);
-        return bookRepository.update(existedBook);
+        return bookRepository.save(existedBook);
     }
 
     @Override
