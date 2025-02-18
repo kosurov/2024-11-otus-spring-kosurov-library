4df01881adf3f508e1c8deeb7b16bc374dffef64
Dmitrii Kosurov <dkosurov@diasoft.ru>
2025-02-19 00:45:57 +0300

    update unit tests

--- spring-02-04/src/test/java/ru/diasoft/library/repository/impl/AuthorRepositoryJpaTest.java
+++ spring-02-04/src/test/java/ru/diasoft/library/repository/impl/AuthorRepositoryJpaTest.java
@@ -5,110 +5,51 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
 import ru.diasoft.library.domain.Author;
-import ru.diasoft.library.exception.AuthorNotFoundException;
+import ru.diasoft.library.repository.AuthorRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Репозиторий для работы с авторами")
 @DataJpaTest
-@Import(AuthorRepositoryJpa.class)
 class AuthorRepositoryJpaTest {
 
     private static final int EXPECTED_QUANTITY_OF_AUTHORS = 3;
-    private static final long NOT_EXISTED_AUTHOR_ID = 100L;
     private static final String NOT_EXISTED_AUTHOR_FULL_NAME = "Пушкин";
     private static final long EXISTED_AUTHOR_ID = 1L;
     private static final String EXISTED_AUTHOR_FULL_NAME = "Агата Кристи";
 
     @Autowired
-    private AuthorRepositoryJpa authorRepositoryJpa;
+    private AuthorRepository authorRepository;
     @Autowired
     private TestEntityManager em;
 
     @DisplayName("Возвращает всех авторов")
     @Test
     void findAll_shouldReturnAllAuthors() {
-        List<Author> authors = authorRepositoryJpa.findAll();
+        List<Author> authors = authorRepository.findAll();
         assertThat(authors).hasSize(EXPECTED_QUANTITY_OF_AUTHORS);
-    }
-
-    @DisplayName("Ищет автора по id")
-    @Test
-    void getById_shouldReturnAuthor() {
-        Author expected = em.find(Author.class, EXISTED_AUTHOR_ID);
-        Author actual = authorRepositoryJpa.getById(expected.getId());
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
-    }
-
-    @DisplayName("Выбрасывает исключение, если автор не найден по id")
-    @Test
-    void getById_shouldThrowException() {
-        assertThatThrownBy(() -> authorRepositoryJpa.getById(NOT_EXISTED_AUTHOR_ID))
-                .isInstanceOf(AuthorNotFoundException.class);
     }
 
     @DisplayName("Сохраняет автора в БД")
     @Test
     void save_shouldSaveAuthor() {
         Author author = new Author(null, NOT_EXISTED_AUTHOR_FULL_NAME);
-        List<Author> authorsBefore = authorRepositoryJpa.findAll();
-        authorRepositoryJpa.save(author);
-        List<Author> authorsAfter = authorRepositoryJpa.findAll();
+        List<Author> authorsBefore = authorRepository.findAll();
+        authorRepository.save(author);
+        List<Author> authorsAfter = authorRepository.findAll();
         assertThat(authorsAfter.size()).isEqualTo(authorsBefore.size() + 1);
-    }
-
-    @DisplayName("Устанавливает id при сохранении автора в БД")
-    @Test
-    void save_shouldSetId() {
-        Author author = new Author(null, NOT_EXISTED_AUTHOR_FULL_NAME);
-        Author actual = authorRepositoryJpa.save(author);
-        assertThat(actual.getId()).isNotNull();
-    }
-
-    @DisplayName("Обновляет автора в БД")
-    @Test
-    void update_shouldUpdateAuthor() {
-        Author existed = em.find(Author.class, EXISTED_AUTHOR_ID);
-        assertThat(existed.getFullName()).isNotEqualTo(NOT_EXISTED_AUTHOR_FULL_NAME);
-        em.detach(existed);
-
-        Author toUpdate = new Author(EXISTED_AUTHOR_ID, NOT_EXISTED_AUTHOR_FULL_NAME);
-
-        authorRepositoryJpa.update(toUpdate);
-
-        Author actual = em.find(Author.class, EXISTED_AUTHOR_ID);
-        assertThat(actual.getFullName()).isEqualTo(NOT_EXISTED_AUTHOR_FULL_NAME);
-    }
-
-    @DisplayName("Удаляет автора из БД по id")
-    @Test
-    void deleteById_shouldDeleteAuthor() {
-        Author author = em.find(Author.class, EXISTED_AUTHOR_ID);
-        assertThat(author).isNotNull();
-        em.detach(author);
-
-        authorRepositoryJpa.deleteById(EXISTED_AUTHOR_ID);
-        Author deleted = em.find(Author.class, EXISTED_AUTHOR_ID);
-        assertThat(deleted).isNull();
     }
 
     @DisplayName("Ищет автора по fullName")
     @Test
     void getByFullName_shouldReturnAuthor() {
         Author expected = em.find(Author.class, EXISTED_AUTHOR_ID);
-        Author actual = authorRepositoryJpa.getByFullName(EXISTED_AUTHOR_FULL_NAME);
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
-    }
-
-    @DisplayName("Выбрасывает исключение, если автор не найден по fullName")
-    @Test
-    void getByFullName_shouldThrowException() {
-        assertThatThrownBy(() -> authorRepositoryJpa.getByFullName(NOT_EXISTED_AUTHOR_FULL_NAME))
-                .isInstanceOf(AuthorNotFoundException.class);
+        Optional<Author> actual = authorRepository.findByFullName(EXISTED_AUTHOR_FULL_NAME);
+        assertThat(actual.isPresent()).isTrue();
+        assertThat(actual.get()).usingRecursiveComparison().isEqualTo(expected);
     }
 }
