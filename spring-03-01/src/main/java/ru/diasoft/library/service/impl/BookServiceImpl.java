7a1b9b39f814a65c5aa471b44937f51b60b17b5e
Dmitrii Kosurov <dkosurov@diasoft.ru>
2025-04-05 04:35:55 +0300

    add web controllers

--- spring-03-01/src/main/java/ru/diasoft/library/service/impl/BookServiceImpl.java
+++ spring-03-01/src/main/java/ru/diasoft/library/service/impl/BookServiceImpl.java
@@ -3,16 +3,18 @@ package ru.diasoft.library.service.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.diasoft.library.exception.BookNotFoundException;
+import ru.diasoft.library.mapper.BookMapper;
 import ru.diasoft.library.repository.BookRepository;
 import ru.diasoft.library.domain.Author;
 import ru.diasoft.library.domain.Book;
 import ru.diasoft.library.domain.Genre;
-import ru.diasoft.library.dto.BookRequestDto;
 import ru.diasoft.library.service.AuthorService;
 import ru.diasoft.library.service.BookService;
 import ru.diasoft.library.service.GenreService;
+import ru.diasoft.library.dto.BookDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -20,45 +22,52 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final AuthorService authorService;
     private final GenreService genreService;
+    private final BookMapper bookMapper;
 
-    public BookServiceImpl(BookRepository bookRepository, AuthorService authorService, GenreService genreService) {
+    public BookServiceImpl(BookRepository bookRepository, AuthorService authorService,
+                           GenreService genreService, BookMapper bookMapper) {
         this.bookRepository = bookRepository;
         this.authorService = authorService;
         this.genreService = genreService;
+        this.bookMapper = bookMapper;
     }
 
     @Override
     @Transactional
-    public Book create(BookRequestDto bookDto) {
-        Author author = authorService.findByFullNameOrCreate(bookDto.getAuthorFullName());
+    public BookDto create(BookDto bookDto) {
+        Author author = authorService.findByFullNameOrCreate(bookDto.getAuthor());
         Genre genre = genreService.findByNameOrCreate(bookDto.getGenre());
         Book book = new Book();
         book.setTitle(bookDto.getTitle());
         book.setAuthor(author);
         book.setGenre(genre);
-        return bookRepository.save(book);
+        return bookMapper.bookToBookDto(bookRepository.save(book));
     }
 
     @Override
-    public Book getById(long id) {
-        return bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
+    public BookDto getById(long id) {
+        return bookRepository.findById(id)
+                .map(bookMapper::bookToBookDto)
+                .orElseThrow(BookNotFoundException::new);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::bookToBookDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     @Transactional
-    public Book update(BookRequestDto bookDto) {
-        Author author = authorService.findByFullNameOrCreate(bookDto.getAuthorFullName());
+    public BookDto update(long id, BookDto bookDto) {
+        Author author = authorService.findByFullNameOrCreate(bookDto.getAuthor());
         Genre genre = genreService.findByNameOrCreate(bookDto.getGenre());
-        Book existedBook = bookRepository.findById(bookDto.getId()).orElseThrow(BookNotFoundException::new);
+        Book existedBook = bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
         existedBook.setTitle(bookDto.getTitle());
         existedBook.setAuthor(author);
         existedBook.setGenre(genre);
-        return bookRepository.save(existedBook);
+        return bookMapper.bookToBookDto(bookRepository.save(existedBook));
     }
 
     @Override
