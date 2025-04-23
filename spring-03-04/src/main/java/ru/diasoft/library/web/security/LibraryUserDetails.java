68130e6d18cfc3bb4f7ac80515e31ecbce9c8f27
Dmitrii Kosurov <dkosurov@diasoft.ru>
2025-04-23 03:03:04 +0300

    add security config

---
+++ spring-03-04/src/main/java/ru/diasoft/library/web/security/LibraryUserDetails.java
@@ -0,0 +1,39 @@
+package ru.diasoft.library.web.security;
+
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class LibraryUserDetails implements UserDetails, CredentialsContainer {
+
+    private String username;
+    private String password;
+
+    public LibraryUserDetails(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}
